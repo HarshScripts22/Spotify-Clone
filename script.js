95b14ed6f0a2fa1e835c82bb3dc781fc024e270e
Harsh Deshmukh <deshmukhharsh2507@gmail.com>
2024-06-27 16:56:24 +0530

    make changes in playbar

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ console.log("here is javascript code");
 async function main() {
   await fetch("http://127.0.0.1:3000/songs/");
 }
+//63670e7b4e2b4bccab06c4013f320256
+//client secret - 6eb485650bab4ba4b64a1c580e8579c5
